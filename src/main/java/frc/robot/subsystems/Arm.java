ba2483806cfc9f30f390914f155b1da20838eb16
NelsonWong2026 <1361400308@k12.hi.us>
2025-01-22 17:32:22 -1000

    worked on mechanism

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -8,37 +8,134 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Arm extends SubsystemBase {
-    private SparkMax armPivot, intakePivot;
-    private SparkMaxConfig armPivotConfig, intakePivotConfig = new SparkMaxConfig();
+    private SparkMax armPivot, intakePivot, intake;
+    private SparkMaxConfig armPivotConfig, intakePivotConfig, intakeConfig = new SparkMaxConfig();
     private RelativeEncoder armPivotEncoder, intakePivotEncoder;
-    private ProfiledPIDController armPivotController =
+    private ProfiledPIDController armPivotPIDController =
         new ProfiledPIDController(
             0, 0, 0,
             new TrapezoidProfile.Constraints(5, 5)
         );
     
+    private ProfiledPIDController intakePivotPIDController =
+        new ProfiledPIDController(
+            0, 0, 0,
+            new TrapezoidProfile.Constraints(5, 5)
+        );
+
+    private ArmFeedforward armFeedforward = 
+        new ArmFeedforward(0, 0, 0);
+
+    private ArmFeedforward intakeFeedforward = 
+        new ArmFeedforward(0, 0, 0);
+    
 
     public Arm() {
+        //set CAN IDs
         armPivot = new SparkMax(Constants.CANIds.kArmPivotID, MotorType.kBrushless);
         intakePivot = new SparkMax(Constants.CANIds.kIntakePivotID, MotorType.kBrushless);
 
+        //create configs
         armPivotConfig
-            .inverted(true)
+            .inverted(false)
             .idleMode(IdleMode.kBrake);
         intakePivotConfig
-            .inverted(true)
+            .inverted(false)
+            .idleMode(IdleMode.kBrake);
+        intakeConfig
+            .inverted(false)
             .idleMode(IdleMode.kBrake);
 
+        //set configs for motors
         armPivot.configure(armPivotConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
         intakePivot.configure(intakePivotConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
+        //gets encoders
         armPivotEncoder = armPivot.getEncoder();
         intakePivotEncoder = intakePivot.getEncoder();
     }
+
+    public void stopArmPivot() {
+        armPivot.set(0);
+    }
+
+    public void stopIntakePivot() {
+        intakePivot.set(0);
+    }
+
+    public void stopIntake() {
+        intake.set(0);
+    }
+
+    public void intake() {
+        intake.set(0.4);
+    }
+
+    public void outtake() {
+        intake.set(-0.4);
+    }
+
+    public void resetArmPivotEncoder() {
+        armPivotEncoder.setPosition(0);
+    }
+
+    public void resetIntakePivotEncoder() {
+        intakePivotEncoder.setPosition(0);
+    }
+
+    public void getArmPivotEncoderPos() {
+        armPivotEncoder.getPosition();
+    }
+
+    public void getIntakePivotEncoderPos() {
+        intakePivotEncoder.getPosition();
+    }
+
+    //sets target and runs arm pivot motor
+    public Command setArmPivotTarget(double target) {
+        return runOnce(
+            () -> {
+                armPivotPIDController.setGoal(target);
+            }
+        )
+        .andThen(
+            run(
+                () -> {
+                    //need to program a way to calculate angle of arm
+                    armPivot.setVoltage(
+                        armPivotPIDController.calculate(armPivotEncoder.getPosition()) 
+                        + armFeedforward.calculate(0, armPivotPIDController.getSetpoint().velocity)
+                    );
+                }
+            )
+        );
+    }
+
+    //sets target and runs intake pivot motor
+    public Command setIntakePivotTarget(double target) {
+        return runOnce(
+            () -> {
+                intakePivotPIDController.setGoal(target);
+            }
+        )
+        .andThen(
+            run(
+                () -> {
+                    armPivot.setVoltage(
+                        intakePivotPIDController.calculate(intakePivotEncoder.getPosition()) 
+                        + intakeFeedforward.calculate(0, armPivotPIDController.getSetpoint().velocity)
+                    );
+                }
+            )
+        );
+    }
+
 }
--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -3,10 +3,15 @@ package frc.robot.subsystems;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -14,18 +19,68 @@ public class Elevator extends SubsystemBase {
     private SparkMax leftElevator, rightElevator;
     private SparkMaxConfig leftElevatorConfig, rightElevatorConfig = new SparkMaxConfig();
 
+    private RelativeEncoder leftElevatorEncoder, rightElevatorEncoder;
+
+    private ProfiledPIDController elevatorPIDController =
+        new ProfiledPIDController(
+            0, 0, 0,
+            new TrapezoidProfile.Constraints(5, 5)
+        );
+
+    private ElevatorFeedforward elevatorFeedforward = 
+        new ElevatorFeedforward(0, 0, 0);
+
     public Elevator() {
+        //set CAN IDs
         leftElevator = new SparkMax(Constants.CANIds.kLeftElevatorID, MotorType.kBrushless);
         rightElevator = new SparkMax(Constants.CANIds.kRightElevatorID, MotorType.kBrushless);
 
+        //create configs
         leftElevatorConfig
-            .inverted(true)
+            .inverted(false)
             .idleMode(IdleMode.kBrake);
         rightElevatorConfig
-            .inverted(true)
-            .idleMode(IdleMode.kBrake);
+            .inverted(false)
+            .idleMode(IdleMode.kBrake)
+            .follow(leftElevator);
 
+        //sets configs
         leftElevator.configure(leftElevatorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
         rightElevator.configure(rightElevatorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+        //gets encoders
+        leftElevatorEncoder = leftElevator.getEncoder();
+        rightElevatorEncoder = rightElevator.getEncoder();
+    }
+
+    public void getLeftElevatorEncoderPos() {
+        leftElevatorEncoder.getPosition();
+    }
+
+    public void resetLeftElevatorEncoder() {
+        leftElevatorEncoder.setPosition(0);
+    }
+
+    public void stopElevator() {
+        leftElevator.set(0);
+    }
+
+    //sets target and runs elevator motor
+    public Command setElevatorTarget(double target) {
+        return runOnce(
+            () -> {
+                elevatorPIDController.setGoal(target);
+            }
+        )
+        .andThen(
+            run(
+                () -> {
+                    leftElevator.setVoltage(
+                        elevatorPIDController.calculate(leftElevatorEncoder.getPosition()) 
+                        + elevatorFeedforward.calculate(elevatorPIDController.getSetpoint().velocity)
+                    );
+                }
+            )
+        );
     }
 }
